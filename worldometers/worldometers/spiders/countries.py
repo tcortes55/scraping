1523523c523f2f48efe7350aa5f0d3afd8c82f1d
tcortes55 <35512873+tcortes55@users.noreply.github.com>
2021-10-10 19:32:42 -0300

    Initial commit

---
+++ worldometers/worldometers/spiders/countries.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class CountriesSpider(scrapy.Spider):
+    name = 'countries'
+    allowed_domains = ['www.worldometers.info']
+    start_urls = ['https://www.worldometers.info/world-population/population-by-country/']
+
+    def parse(self, response):
+        countries = response.xpath('//td/a')
+
+        for country in countries:
+            name = country.xpath(".//text()").get()
+            link = country.xpath(".//@href").get()
+
+            # absolute_url = f"https://www.worldometers.info{link}"
+            # absolute_url = response.urljoin(link)
+            # yield scrapy.Request(url=absolute_url)
+
+            yield response.follow(link)
