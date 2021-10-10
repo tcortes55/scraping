3b9c1b2bace15ae7579c18ca52a1b64547a49346
tcortes55 <35512873+tcortes55@users.noreply.github.com>
2021-10-10 20:00:19 -0300

    Get population by country per year

--- worldometers/worldometers/spiders/countries.py
+++ worldometers/worldometers/spiders/countries.py
@@ -12,8 +12,8 @@ class CountriesSpider(scrapy.Spider):
         countries = response.xpath('//td/a')
 
         for country in countries:
-            name = country.xpath(".//text()").get()
-            link = country.xpath(".//@href").get()
+            name = country.xpath('.//text()').get()
+            link = country.xpath('.//@href').get()
 
             # absolute_url = f"https://www.worldometers.info{link}"
             # absolute_url = response.urljoin(link)
@@ -22,4 +22,11 @@ class CountriesSpider(scrapy.Spider):
             yield response.follow(url=link, callback=self.parse_country)
 
     def parse_country(self, response):
-        logging.info(response.url)
+        rows = response.xpath('(//table[@class="table table-striped table-bordered table-hover table-condensed table-list"])[1]/tbody/tr')
+        for row in rows:
+            year = row.xpath('.//td[1]/text()').get()
+            population = row.xpath('.//td[2]/strong/text()').get()
+            yield {
+                'year': year,
+                'population': population
+            }
