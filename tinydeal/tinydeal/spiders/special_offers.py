9cc580181e418f1f0907a7e554be46763254204f
tcortes55 <35512873+tcortes55@users.noreply.github.com>
2021-10-11 16:15:43 -0300

    'Tinydeal' spider

---
+++ tinydeal/tinydeal/spiders/special_offers.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class SpecialOffersSpider(scrapy.Spider):
+    name = 'special_offers'
+    allowed_domains = ['web.archive.org']
+    start_urls = ['https://web.archive.org/web/20190225123327/https://www.tinydeal.com/specials.html']
+
+    def parse(self, response):
+        products = response.xpath('//ul[@class="productlisting-ul"]/div/li')
+        for product in products:
+            yield {
+                'title': product.xpath('.//a[@class="p_box_title"]/text()').get(),
+                'discounted_price': product.xpath('.//div[@class="p_box_price"]/span[@class="productSpecialPrice fl"]/text()').get(),
+                'original_price': product.xpath('.//div[@class="p_box_price"]/span[@class="normalprice fl"]/text()').get()
+            }
