da67ea56431639d87c8a354756cff337ca2bfa38
tcortes55 <35512873+tcortes55@users.noreply.github.com>
2021-10-11 22:04:41 -0300

    Scraping other data

--- glasses/glasses/spiders/bestsellers.py
+++ glasses/glasses/spiders/bestsellers.py
@@ -11,5 +11,8 @@ class BestsellersSpider(scrapy.Spider):
         glasses = response.xpath('//div[@id="product-lists"]/div[contains(@class, "product-list-item")]')
         for glass in glasses:
             yield {
-                'product_url': glass.xpath('.//descendant::a[not(contains(@class, "none"))]/img[contains(@class, "product-img-default")]/@data-src').get()
+                'product_url': glass.xpath('.//descendant::a[contains(@class, "product-title") and not(contains(@class, "none"))]/@href').get(),
+                'product_image_link': glass.xpath('.//descendant::a[not(contains(@class, "none"))]/img[contains(@class, "product-img-default")]/@data-src').get(),
+                'product_name': glass.xpath('.//descendant::a[contains(@class, "product-title") and not(contains(@class, "none"))]/@title').get(),
+                'product_price': glass.xpath('.//descendant::div[contains(@class, "p-price") and not(contains(@class, "none"))]/div[not(contains(@class, "none"))]/span/text()').get()
             }
